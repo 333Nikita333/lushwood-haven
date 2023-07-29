48bf50c480612660b5b07be707162c1d16afa63a
333Nikita333 <nikitosfox333@gmail.com>
2023-07-29 21:27:06 +0300

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
   plugins: [react()],
   server: {
     port: 4000,
