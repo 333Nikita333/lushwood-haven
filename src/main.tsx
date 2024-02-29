619e472f41fb360a4fafabb7f35330de94ce62f2
333Nikita333 <nikitosfox333@gmail.com>
2024-02-29 16:03:51 +0200

    some fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import App from './App.tsx';
-import GlobalStyle from './styles/GlobalStyle.ts';
-import theme from './styles/theme.ts';
+import App from './App';
+import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
